5868662b56eaa5636533825919557e64bb49e861
ericc <ericc@159.31.66.141>
2023-10-13 16:31:16 +0200

    Opérations pile

--- Calculatrice/src/Model/CalculatorModel.java
+++ Calculatrice/src/Model/CalculatorModel.java
@@ -3,8 +3,61 @@ package Model;
 import java.util.Stack;
 
 public class CalculatorModel implements CalculatorModelInterface {
-	private String accu;
+	Stack<Double> memory = new Stack<Double>();
+	double accu;
 	
-	
+	public void add() {
+		double x = memory.pop();
+		double y = memory.pop();
+		memory.push(x+y);
+	}
+
+	public void subtract() {
+		double x = memory.pop();
+		double y = memory.pop();
+		memory.push(x-y);		
+	}
+
+	public void multiply() {
+		double x = memory.pop();
+		double y = memory.pop();
+		memory.push(x*y);		
+	}
+
+	public void divide() {
+		double x = memory.pop();
+		double y = memory.pop();
+		memory.push(x/y);		
+	}
+
+	public void opposite() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void push() {
+		memory.push(accu);		
+	}
+
+	public void pop() {
+		
+	}
+
+	public void drop() {
+		memory.pop();		
+	}
+
+	public void swap() {
+		double x = memory.pop();
+		double y = memory.pop();
+		memory.push(x);
+		memory.push(y);
+	}
+
+	public void clear() {
+		while (memory.empty() == false) {
+			memory.pop();
+		}
+	}
 	
 }
