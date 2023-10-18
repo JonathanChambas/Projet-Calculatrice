ac25f0c7c1dccf9cd4d639de34fda2cd4898cd64
jbcel <jbcel@LAPTOP-A9B709G3>
2023-10-18 15:22:25 +0200

    Boutons

--- Javafx1/src/View/Buttons.java
+++ Javafx1/src/View/Buttons.java
@@ -3,12 +3,24 @@ package View;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBase;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class Buttons {
+		
+
+	Button btAdd = new Button("+");
+	Button btSubstract = new Button("-");
+	Button btMultiply = new Button("×");
+	Button btDivide = new Button("/");
+	Button btEnter = new Button("Enter");
 	
 
-	Buttons b1 = new Buttons();
-	Label lb1 = new Label("1");
-	
+	//clavier
+	GridPane gp = new GridPane();
+	for (int i=0; i<3; i++) {
+		Button b = new Button();
+		gp.add(b, i, 1);
+	}
 	}
 
