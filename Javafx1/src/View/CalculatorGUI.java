73a650551c0b08e1986e534dbefddab8c226cfc0
ericc <ericc@159.31.66.141>
2023-10-18 15:44:12 +0200

    Base interface

--- Javafx1/src/View/CalculatorGUI.java
+++ Javafx1/src/View/CalculatorGUI.java
@@ -1,6 +1,30 @@
 package View;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
 public class CalculatorGUI extends javafx.scene.Scene {
 	
+	BorderPane borderPane = new BorderPane();
+
+	Button btAdd = new Button("+");
+	Button btSubstract = new Button("-");
+	Button btMultiply = new Button("×");
+	Button btDivide = new Button("/");
+	Button btEnter = new Button("Enter");
+	
+
+	//clavier
+	GridPane gp = new GridPane();
+	for (int l=0; l<3; l++) { //parcours des lignes
+		for (int c=0; c<3; c++) { // parcours des colonnes
+			Button b = new Button();
+			gp.add(b, c, l);
+	}
+		}
+	borderPane.setBottom(gp);
+	
 	String accu;
+	}
 }
